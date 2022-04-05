3b1d030469cb1fcf02b2d414417bda34f6e05eb6
girgisezzat <81932443+girgisezzat@users.noreply.github.com>
2022-04-05 13:50:11 +0200

    TodoApp

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
           create: (BuildContext context) =>AppCubit()..createDatabase(),
         )
       ],
-      //TodoApp
+      //TodoApplication
       child:MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: lightTheme,
